2a1e662d2f629a4194c8a69dd554bd57e395b298
unknown <marik.3000@hotmail.com>
2018-03-05 14:23:57 +0100

    add

---
+++ ServerClient/Assets/Scrpts/Client.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine.UI;
+using System.Net.Sockets;
+using UnityEngine;
+using System;
+
+
+public class Client : MonoBehaviour
+{
+
+
+    private bool socketReady;
+    private TcpClient socket;
+    private NetworkStream stream;
+    private StreamWriter writer;
+    private StreamReader reader;
+
+    public void ConnectToServer()
+    {
+        if (socketReady)
+            return;
+        string host = "10.50.131.78";
+        int port = 50945;
+
+
+
+        try
+        {
+            socket = new TcpClient(host, port);
+            stream = socket.GetStream();
+            writer = new StreamWriter(stream);
+            reader = new StreamReader(stream);
+           
+            socketReady = true;
+
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Socket error; " + e.Message);
+        }
+
+    }
+    private void Update()
+    {
+        if (socketReady)
+        {
+            if (stream.DataAvailable)
+            {
+                
+                string data = reader.ReadLine();
+                Debug.Log("from server: " + data);
+
+            }
+        }
+    }
+}
+
---
+++ ServerClient/Assets/Scrpts/Server.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class Server : MonoBehaviour
+{
+
+    public int port = 50945;
+    TcpListener listener;
+    int counter = 0;
+    private TcpClient c;
+    private void Start()
+    {
+
+
+        try
+        {
+            listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+       
+            Debug.Log("Server has been started on port" + port.ToString());
+                
+
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Socket error" + e.Message);
+
+        }
+    }
+
+
+    public void Update()
+       
+    {
+            if (listener.Pending())
+        {
+            c = listener.AcceptTcpClient();
+            counter++;
+            Debug.Log("#" + counter + " Connected!");
+            
+
+
+            StreamWriter sw = new StreamWriter(c.GetStream());
+
+            sw.WriteLine("hello!");
+            sw.Flush();
+
+   
+
+
+
+       
+        }
+        }
+
+    }
+
